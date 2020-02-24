cd7217ac607790a6a0c9ddd923345b6631700545
Brian Robinson <elderbrianrobinson@gmail.com>
2020-02-24 17:05:03 -0500

    Music rendering

--- backend/music-service.js
+++ backend/music-service.js
@@ -2,80 +2,6 @@ const verovio = require('verovio')
 var vrvToolkit = new verovio.toolkit()
 
 function renderMusic(music) {
-    /*
-    let data =
-    `<?xml version="1.0" encoding="UTF-16" ?>
-    <mei meiversion="3.0.0" xmlns="http://www.music-encoding.org/ns/mei" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <meiHead></meiHead>
-    <music>
-        <body>
-            <mdiv>
-                <score>
-                    <scoreDef meter.count="4" meter.unit="4">
-                        <staffGrp>
-                            <staffDef clef.line="2" clef.shape="G" key.mode="major" key.sig="0" lines="5" n="1"></staffDef>
-                        </staffGrp>
-                    </scoreDef>
-                    <section>
-                        <measure n="1">
-                            <staff n="1">
-                                <layer n="1">
-                                    <chord dur="2" stem.dir="up">
-                                        <note dur="2" oct="4" pname="d"/>
-                                        <note dur="2" oct="4" pname="f"/>
-                                        <note dur="2" oct="5" pname="c"/>
-                                        <note dur="2" oct="5" pname="e"/>
-                                    </chord>
-                                    <chord dur="2" stem.dir="up">
-                                        <note dur="2" oct="4" pname="d">
-                                            <accid accid="s"/>
-                                        </note>
-                                        <note dur="2" oct="4" pname="f">
-                                            <accid accid="s"/>
-                                        </note>
-                                        <note dur="2" oct="5" pname="c">
-                                            <accid accid="s"/>
-                                        </note>
-                                        <note dur="2" oct="5" pname="e">
-                                            <accid accid="s"/>
-                                        </note>
-                                    </chord>
-                                </layer>
-                            </staff>
-                        </measure>
-                        <measure n="1">
-                            <staff n="1">
-                                <layer n="1">
-                                    <chord dur="2" stem.dir="up">
-                                        <note dur="2" oct="4" pname="d"/>
-                                        <note dur="2" oct="4" pname="f"/>
-                                        <note dur="2" oct="5" pname="c"/>
-                                        <note dur="2" oct="5" pname="e"/>
-                                    </chord>
-                                    <chord dur="2" stem.dir="up">
-                                        <note dur="2" oct="4" pname="d">
-                                            <accid accid="s"/>
-                                        </note>
-                                        <note dur="2" oct="4" pname="f">
-                                            <accid accid="s"/>
-                                        </note>
-                                        <note dur="2" oct="5" pname="c">
-                                            <accid accid="s"/>
-                                        </note>
-                                        <note dur="2" oct="5" pname="e">
-                                            <accid accid="s"/>
-                                        </note>
-                                    </chord>
-                                </layer>
-                            </staff>
-                        </measure>
-                    </section>
-                </score>
-            </mdiv>
-        </body>
-    </music>
-    </mei>`;
-    */
 
    var data = `<measure n="1"><staff n="1"><layer n="1">`
    for (note of music.notes) {
@@ -124,8 +50,7 @@ function renderMusic(music) {
     </music>
     </mei>`
 
-    //var svg = vrvToolkit.renderData(data, {})
-    var svg = vrvToolkit.renderData(data, { pageWidth: 1000, adjustPageHeight: 1});
+    var svg = vrvToolkit.renderData(data, { pageWidth: 1000, adjustPageHeight: true });
     return svg
 }
 
